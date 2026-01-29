686384692dbe9310bdc84b9a433a0c6c0941cba7
Seth Harrington <sbgha.09@gmail.com>
2026-01-29 13:07:32 -0600

    git ignore updated

---
+++ handcrafted/.next/dev/static/chunks/fullstack_handcrafted_handcrafted_src_app_favicon_ico_mjs_c92e1705._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/d7078_next_dist_363806ca._.js"
+],
+    source: "dynamic"
+});
